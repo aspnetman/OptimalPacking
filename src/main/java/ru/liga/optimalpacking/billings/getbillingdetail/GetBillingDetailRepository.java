920dcbf23bc4a2db2fb63e1db6b220d38ea93659
Maksim Kovalenko <maksim.kovalenko@hoff.ru>
2025-01-22 18:05:35 +0300

    Исправление замечания после ревью

--- src/main/java/ru/liga/optimalpacking/billings/getbillingdetail/GetBillingDetailRepository.java
+++ src/main/java/ru/liga/optimalpacking/billings/getbillingdetail/GetBillingDetailRepository.java
@@ -1,11 +1,13 @@
 package ru.liga.optimalpacking.billings.getbillingdetail;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.liga.optimalpacking.shared.BillingRepository;
 import ru.liga.optimalpacking.shared.entities.Billing;
 
 import java.util.List;
 
+@Component
 @RequiredArgsConstructor
 public class GetBillingDetailRepository {
 
